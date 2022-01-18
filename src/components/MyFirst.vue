bd5be2c3b910e6d67f0eeca9d046c3bf9f996b9f
e-schultz <evan@schultz.codes>
2022-01-18 14:53:19 -0500

    silent failure
    I haven't imported the text input - so it's just not displaying,
    
    but also no errors that I'm seeing
    
    I lied
    
    - open dev tools
    - console will display a warning that TextInput was referenced

--- src/components/MyFirst.vue
+++ src/components/MyFirst.vue
@@ -1,13 +1,47 @@
 <script setup>
-import { ref } from "Vue"
-import DynamicOne from "./DynamicOne.vue"
-import DynamicTwo from "./DynamicTwo.vue"
+import { ref } from "Vue";
+import DynamicOne from "./DynamicOne.vue";
+import DynamicTwo from "./DynamicTwo.vue";
+
 let message = "Test";
-let showWich = ref("DynamicOne")
+let schema = [
+  /*{
+    fieldType: "SelectList",
+    name: "title",
+    multi: false,
+    label: "Title",
+    options: ["Ms", "Mr", "Mx", "Dr", "Madam", "Lord"],
+  },*/
+  {
+    fieldType: "TextInput",
+    placeholder: "First Name",
+    label: "First Name",
+    name: "firstName",
+  },
+  /*{
+    fieldType: "TextInput",
+    placeholder: "Last Name",
+    label: "Last Name",
+    name: "lastName",
+  },
+  {
+    fieldType: "NumberInput",
+    placeholder: "Age",
+    name: "age",
+    label: "Age",
+    minValue: 0,
+  },*/
+];
+let showWich = ref("DynamicOne");
 </script>
 <template>
-
-Showing .... {{showWich}}
-<component :is="showWich"/>
-<button @click="showWich = (showWich == 'DynamicOne') ? 'DynamicTwo' : 'DynamicOne'">Swap</button>
+  <div>
+    Showing .... {{ showWich }}
+    <component :is="showWich" />
+    <button @click="showWich = showWich == 'DynamicOne' ? 'DynamicTwo' : 'DynamicOne'">
+      Swap
+    </button>
+    <br />
+    <component :is="TextInput" v-bind="schema[0]" />
+  </div>
 </template>
---
+++ src/components/TextInput.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps({
+  label: String,
+  placeholder: String,
+  name: String,
+});
+</script>
+<template>
+  <label>{{ label }}</label>
+  <input type="text" :name="name" :placeholder="placeholder" />
+</template>
